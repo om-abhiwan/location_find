26e5ca047cb6e049e35a9fe01da7edf728738141
om-abhiwan <om.abhiwan@gmail.com>
2024-05-07 13:35:48 +0530

    Adding pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as serviceWorker from "./ServiceWorker";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+serviceWorker.register();
