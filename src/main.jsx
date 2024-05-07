a457d36e41f466c6848f3bcfa5b55d9e71035e09
om-abhiwan <om.abhiwan@gmail.com>
2024-05-07 13:40:53 +0530

    PWA Updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as serviceWorker from "./ServiceWorker";
 
+import * as PWA from "./Pwa.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 
-serviceWorker.register();
+PWA.register()
